030a1b83155bb2fb0d7a6ba2544dc87b79352709
Jarryd Ross <jarrydschool1234@gmail.com>
2024-02-08 10:15:14 -0500

    Put Implementation for Comment

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -7,6 +7,7 @@ using api.Dtos.Comment;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 
 namespace api.Controllers
 {
@@ -49,9 +50,22 @@ namespace api.Controllers
             }
             var commentModel = commentDto.ToCommentFromCreate(stockId);
             await _commentRepo.CreateAsync(commentModel);
-            return CreatedAtAction(nameof(GetById), new {id = commentModel}, commentModel.ToCommentDto());
+            return CreatedAtAction(nameof(GetById), new {id = commentModel.Id}, commentModel.ToCommentDto());
 
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+            if (comment == null)
+            {
+                return NotFound ("Comment not found");
+            }
+            return Ok(comment.ToCommentDto());
+        }
+
 
         [HttpDelete]
         [Route("{id}")]
